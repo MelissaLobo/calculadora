ac31e1050f73cb445ef726b2c8acd8191b4a4301
Melissa Lobo <mellobomel@gmail.com>
2015-04-23 19:48:11 -0300

    Adicionando modulos de teste

---
+++ testes.js
@@ -0,0 +1,25 @@
+/**
+ * @author melissaLobo mellobomel@gmail.com
+ */
+
+module("Primeiro Modulo de Teste");
+
+test("Teste da função Somar", function() {
+	
+})
+
+module("Segundo Modulo de Teste");
+test("Teste da função Subitrair", function() {
+	
+})
+
+module("Terceiro Modulo de Teste");
+test("Testando a função Dividir", function() {
+	
+})
+
+module("Quarto Modulo de Teste");
+test("Testando a função Multiplicar", function() {
+	
+})
+
