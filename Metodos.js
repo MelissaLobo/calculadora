20977c0fa61609a906341ca1b388eb3241576b9c
Melissa Lobo <mellobomel@gmail.com>
2015-04-23 19:24:47 -0300

    Adicionando funcoes da calculadora

---
+++ Metodos.js
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+
+function somar(numero1, numero2){
+	if(isNumeric(numero1) && isNumeric(numero2)){
+		return parseFloat(numero1) + parseFloat(numero2);
+	}
+	return 0;
+}
+
+function subtrair(numero1, numero2){
+	if(isNumeric(numero1) && isNumeric(numero2)){
+		return numero1 - numero2;
+	}
+	return 0;
+}
+
+function multiplicar(numero1, numero2){
+	if(isNumeric(numero1) && isNumeric(numero2)){
+		return numero1 * numero2;
+	}
+	return 0;
+}
+
+function dividir(numero1, numero2){
+	if(isNumeric(numero1) && isNumeric(numero2)){
+		return numero1 / numero2;
+	}
+	return 0;
+}
+
+//function isNumeric(str){
+//  var er = /^[0-9]+$/;
+//  return (er.test(str));
+//}
