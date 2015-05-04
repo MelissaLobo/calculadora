9bf62ccae3a99ad9356ab8d7b7a23644d0f9426a
Melissa Lobo <mellobomel@gmail.com>
2015-05-04 10:59:34 -0300

    Adicionando estilo

--- Metodos.js
+++ Metodos.js
@@ -2,33 +2,33 @@
  * @author melissaLobo mellobomel@gmail.com
  */
 function somar() {
-		var n1 = document.getElementById("txtN1").value;
-		var n2 = document.getElementById("txtN2").value;
-		var resultado = parseInt(n1) + parseInt(n2);
-		document.getElementById("txtResultado").value = resultado;
-		document.getElementById("txtResultado2").innerHTML = resultado;
-	}
+	var n1 = document.getElementById("txtN1").value;
+	var n2 = document.getElementById("txtN2").value;
+	var resultado = parseInt(n1) + parseInt(n2);
+	document.getElementById("txtResultado").value = resultado;
+	document.getElementById("txtResultado2").innerHTML = resultado;
+}
 
-	function subtrair() {
-		var n1 = document.getElementById("txtN1").value;
-		var n2 = document.getElementById("txtN2").value;
-		var resultado = parseInt(n1) - parseInt(n2);
-		document.getElementById("txtResultado").value = resultado;
-		document.getElementById("txtResultado2").innerHTML = resultado;
-	}
+function subtrair() {
+	var n1 = document.getElementById("txtN1").value;
+	var n2 = document.getElementById("txtN2").value;
+	var resultado = parseInt(n1) - parseInt(n2);
+	document.getElementById("txtResultado").value = resultado;
+	document.getElementById("txtResultado2").innerHTML = resultado;
+}
 
-	function multiplicar() {
-		var n1 = document.getElementById("txtN1").value;
-		var n2 = document.getElementById("txtN2").value;
-		var resultado = parseInt(n1) * parseInt(n2);
-		document.getElementById("txtResultado").value = resultado;
-		document.getElementById("txtResultado2").innerHTML = resultado;
-	}
+function multiplicar() {
+	var n1 = document.getElementById("txtN1").value;
+	var n2 = document.getElementById("txtN2").value;
+	var resultado = parseInt(n1) * parseInt(n2);
+	document.getElementById("txtResultado").value = resultado;
+	document.getElementById("txtResultado2").innerHTML = resultado;
+}
 
-	function dividir() {
-		var n1 = document.getElementById("txtN1").value;
-		var n2 = document.getElementById("txtN2").value;
-		var resultado = parseFloat(n1) / parseFloat(n2);
-		document.getElementById("txtResultado").value = resultado;
-		document.getElementById("txtResultado2").innerHTML = resultado;
-	}
+function dividir() {
+	var n1 = document.getElementById("txtN1").value;
+	var n2 = document.getElementById("txtN2").value;
+	var resultado = parseFloat(n1) / parseFloat(n2);
+	document.getElementById("txtResultado").value = resultado;
+	document.getElementById("txtResultado2").innerHTML = resultado;
+}
